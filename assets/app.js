10d07d5bca4e6141e7e15497c22fed19eb56b296
Riviera77 <103321314+Riviera77@users.noreply.github.com>
2024-06-05 17:35:17 +0200

    update

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,3 @@
-import { Octokit } from 'https://esm.sh/octokit';
+import { Home } from './modules/Home.js';
 
-//Instanciation de l'objet Octokit
-const octokit = new Octokit({});
-console.log(octokit);
-
-//octokit.rest.repos => provient du SDK
-// get => Méthode du SDK pour récupérer ici les repos
-octokit.rest.repos
-	.get({
-		owner: 'Riviera77',
-		repo: 'RepoGithub',
-	}) // On récupère un seul repo
-	.then(({ data }) => {
-		console.log('Repo récupéré', data);
-		console.log(data.url);
-	});
+new Home();
