915ea002a9bbe9bee038befc265becaf01cc58af
Riviera77 <103321314+Riviera77@users.noreply.github.com>
2024-06-07 10:36:27 +0200

    update

--- assets/modules/Home.js
+++ assets/modules/Home.js
@@ -1,9 +1,14 @@
+import { Octokit, App } from 'https://esm.sh/octokit';
+
 class Home {
 	constructor() {
 		this.descriptionHTML = document.querySelector('.js-home-description');
 		this.profilHTML = document.querySelector('.js-home-profil-url');
 		this.avatarHTML = document.querySelector('.js-home-avatar');
-		this.data = {};
+
+		this.projectsTitle = document.querySelectorAll('.js-home-project-title');
+		this.projectsDescription = document.querySelectorAll('.js-home-project-description');
+		this.projectsTagsContainer = document.querySelectorAll('.js-home-project-tags-container');
 
 		this.init();
 	}
@@ -11,33 +16,37 @@ class Home {
 	init() {
 		// Récupérer les informations du profil depuis l'API
 		this.getUserInformations();
+		this.getReposInformations();
 	}
 
 	getUserInformations() {
-		// Récupérer les informations du profil depuis l'API
-
+		// API #1 : Récupérer les informations du profil depuis l'API avec fetch
 		fetch('https://api.github.com/users/Riviera77')
 			.then((response) => response.json())
 			.then((data) => {
-				this.APIdata = data;
-				this.updateContent();
-
-				this.descriptionHTML.innerHTML = data.bio;
-				this.profilHTML.innerHTML = data.html_url;
-				this.avatarHTML.src = data.avatar_url;
+				this.updateHTMLUser(data);
 			})
 			.catch((error) => {
 				console.error('Erreur lors de la récupération des informations du profil', error);
 			});
 	}
 
-	updateContent() {
+	async getReposInformations() {
+		// API #2 : Récupérer les informations du profil depuis l'API avec Octokit JS + await/async
+		//URL de l'API classique : https://api.github.com/users/Riviera77/repos
+		const octokit = new Octokit();
+		const response = await octokit.request('GET /users/Riviera77/repos');
+		const data = response.data;
+		console.log(data);
+	}
+
+	updateHTMLUser(APIdata) {
 		// Afficher la description de mon profil Github
-		this.descriptionHTML.textContent = this.APIdata.bio;
+		this.descriptionHTML.textContent = APIdata.bio;
 		// Afficher l'url de mon profil Github
-		this.profilHTML.setAttribute('href', this.APIdata.html_url);
+		this.profilHTML.setAttribute('href', APIdata.html_url);
 		// Afficher mon avatar Github
-		this.avatarHTML.setAttribute('src', this.APIdata.avatar_url);
+		this.avatarHTML.setAttribute('src', APIdata.avatar_url);
 	}
 }
 
